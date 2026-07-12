cee74645703f8601172bbe3ff888db1d7c36bff5
Magnu-Shaak <mn2patel02@gmail.com>
2026-07-12 04:32:12 +0000

    Configure OLED settings

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,10 +1,29 @@
-import board
+import Firmware.board as board
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 
+#Initialize
 keyboard = KMKKeyboard()
 
+#OLED
+import busio
+from kmk.extensions.display import Display, TextEntry, ImageEntry
+from kmk.extensions.display.ssd1306 import SSD1306
+
+i2c_bus = busio.I2C(board.D5, board.D4)     # D4 - SDA, D5 - SCL
+driver = SSD1306(
+    i2c = i2c_bus,
+)
+display = Display(
+    display=driver,
+    width=128,
+    height=32,
+    flip = False, 
+    brightness=0.8, 
+)
+
+#Keyboard Layout
 keyboard.col_pins = (board.D7, board.D8, board.D9, board.D6)        # D7 D8, D9, D6
 keyboard.row_pins = (board.D10, board.D0, board.D2, board.D3)       # D10, D0, D2, D3
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
